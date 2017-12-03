4ea50d2cf32870316fdac2a1a1c8a705865e89e1
Ezequiel Smucler <ezequiels.90@gmail.com>
2017-12-03 10:23:28 -0800

    try this

--- src/config.h
+++ src/config.h
@@ -1,5 +1,20 @@
 #include "autoconfig.h"
 
+#ifdef HAVE_INTTYPES_H
+#   include <inttypes.h>
+#endif
+
+#ifdef HAVE_STDINT_H
+#   include <stdint.h>
+#endif
+
+#ifndef __cplusplus
+#   undef RESTRICT
+#   define RESTRICT restrict
+#elif !defined(RESTRICT)
+#   define RESTRICT
+#endif
+
 #ifndef DEBUG
 #   define ARMA_NO_DEBUG
 #endif
